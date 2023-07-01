e415768f8ae1a79e79d6f37042b24d2012c341c4
Dustin Stacy <dustin.stacy@outlook.com>
2023-07-01 11:50:03 -0400

    Disable PackContents back button during Onboarding

--- client/src/pages/OpenPacks/components/PackContents/PackContents.jsx
+++ client/src/pages/OpenPacks/components/PackContents/PackContents.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 
 import { Button, Card } from '@components'
+import { useGlobalContext } from '@context'
 
 import './PackContents.scss'
 
 // Render contents of opened pack and button to return
-const PackContents = ({ packContents, setPackContents }) => (
-    <div className='packs-contents fill center'>
-        {packContents?.map((data) => (
-            <Card key={data._id} card={data} isShowing />
-        ))}
-        <Button label='Go Back' onClick={() => setPackContents(null)} />
-    </div>
-)
+const PackContents = ({ packContents, setPackContents }) => {
+    const { user } = useGlobalContext()
+    const stage = user?.onboardingStage
+
+    return (
+        <div className='packs-contents fill center'>
+            {packContents?.map((data) => (
+                <Card key={data._id} card={data} isShowing />
+            ))}
+            <Button
+                label='Go Back'
+                onClick={() => setPackContents(null)}
+                disabled={stage < 5}
+            />
+        </div>
+    )
+}
 
 export default PackContents
