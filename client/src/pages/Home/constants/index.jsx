ed2ba78ce4d49c35a168551640ef3260c60292ad
Dustin Stacy <dustin.stacy@outlook.com>
2023-07-04 14:15:51 -0400

    Add fix for incorrect Home panel route

--- client/src/pages/Home/constants/index.jsx
+++ client/src/pages/Home/constants/index.jsx
@@ -64,7 +64,7 @@ export const mainPanels = [
     {
         className: 'market',
         type: 'main',
-        to: '/packs',
+        to: '/market',
         text: 'Purchase packs',
         header: 'Market',
     },
