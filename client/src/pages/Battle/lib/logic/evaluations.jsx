11c7787521e32528fa2ef2440819305fe0ef9525
Andrew <fleming-andrew@protonmail.com>
2023-07-04 00:16:04 -0400

    add comments

--- client/src/pages/Battle/lib/logic/evaluations.jsx
+++ client/src/pages/Battle/lib/logic/evaluations.jsx
@@ -1,3 +1,11 @@
+/**
+ * Evaluates single (1-1) card values between active player and opponent
+ * @param {object} dir - direction of active card relative to opponent card
+ * @param {string} color - color of the active player (red or blue)
+ * @param {dict} ruleset - (TMP) dictionary with information regarding current game rules
+ * @param {number} v1 - active player's card value adjacent to opponent's card value
+ * @param {number} v2 - opponent's card value adjacent to active player's card value
+ */
 export const evaluate = (dir, color, ruleset, v1, v2) => {
     const isLt = v1 < v2
     const isGt = v1 > v2
@@ -11,6 +19,22 @@ export const evaluate = (dir, color, ruleset, v1, v2) => {
     }
 }
 
+/**
+ * Evaluates dual card values between active player and opponent. Specifically,
+ * this checks for instances of "same" and/or "plus".
+ * "Same" occurs when two (or more) of the active player's card values exactly matches
+ * the values of which they're facing e.g. (2:2, 8:8, 1:1, etc.)
+ * "Plus" occurs when the sum of one of its values with the value it's facing equals
+ * the sum of another direction's values with the value it's facing.
+ * TODO: add visuals to help explain all rules in MD files
+ * @param {object} dir1 - direction of active card relative to opponent card
+ * @param {object} dir2 - second direction of active card relative to opponent card
+ * @param {string} color - color of the active player (red or blue)
+ * @param {dict} ruleset - (TMP) dictionary with information regarding current game rules
+ * @param {Array} p1 - Two-element array of the indexed card values to evaluate against the opponent
+ * @param {Array} p2 - Two-element array of the indexed card values of which the active player's
+ *                     cards will be evaluated against
+ */
 export const evaluateSameAndPlus = (dir1, dir2, color, ruleset, p1, p2) => {
     const isSame = p1.toString() == p2.toString()
     const isPlus = [p1[0] + p2[0]].toString() == [p1[1] + p2[1]].toString()
@@ -28,21 +52,26 @@ export const evaluateSameAndPlus = (dir1, dir2, color, ruleset, p1, p2) => {
 // Helper functions
 //
 
+// Returns true if the relative direction (dir) is facing another color
+// Expects dir.color and color to either be red or blue
 const isOpponent = (dir, color) => {
     return dir.color !== color
 }
 
+// Changes the color of the target
 const capture = (dir, color) => {
     dir.color = color
     dir.captured = !dir.captured
 }
 
+// Captures if 'res' is true
 const captureIfTrue = (res, dir, color) => {
     if (res) {
         capture(dir, color)
     }
 }
 
+// Captures if and only if the target is not the same color as the active player
 const captureIfOpponent = (dir, color) => {
     if (isOpponent(dir, color)) {
         capture(dir, color)
@@ -62,5 +91,6 @@ export const exportedForTesting = {
     isOpponent,
     capture,
     captureIfTrue,
-    captureIfOpponent
+    captureIfOpponent,
+    captureOpponentCardsIfTrue
 }
