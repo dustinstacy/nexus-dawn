d67191cf820021d4435196945eee4390892e9a79
Dustin Stacy <dustin.stacy@outlook.com>
2023-07-10 11:47:24 -0400

    Add AquaRegia and AquaRegiaSuperior functionality

--- client/src/pages/Forge/components/Modifiers/components/AquaRegia/AquaRegia.jsx
+++ client/src/pages/Forge/components/Modifiers/components/AquaRegia/AquaRegia.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { Button, Card } from '@components'
+import { useGlobalContext } from '@context'
+
+import { updateCardValues } from '../../../../api'
 import './AquaRegia.scss'
 
-const AquaRegia = () => {
-    return <div>AquaRegia</div>
+const AquaRegia = ({ selectedCard, setModificationComplete }) => {
+    const { getUserCards } = useGlobalContext()
+
+    const [selectedCardValues, setSelectedCardValues] = useState([
+        ...selectedCard.values,
+    ])
+    const [rotated, setRotated] = useState(false)
+
+    let updatedCardValues = [...selectedCardValues]
+
+    const rotateValues = () => {
+        updatedCardValues.unshift(updatedCardValues.pop())
+        setSelectedCardValues(updatedCardValues)
+        setRotated(true)
+    }
+
+    const completeMod = async () => {
+        await updateCardValues(selectedCard, updatedCardValues)
+        await getUserCards()
+        setModificationComplete(true)
+    }
+
+    const reset = () => {
+        setSelectedCardValues(selectedCard.values)
+        setRotated(false)
+    }
+
+    return (
+        <div className='Regia center fill'>
+            <div className='start-column'>
+                <Button
+                    label='Rotate Values'
+                    onClick={() => rotateValues()}
+                    disabled={rotated}
+                />
+                <div className='panel card-select center'>
+                    <div className='selected-card center fill'>
+                        <Card card={selectedCard} isShowing />
+                    </div>
+                    {updatedCardValues?.map((value, i) => (
+                        <div
+                            key={value + i * 10}
+                            className={`value-${i} box center `}
+                        >
+                            {value}
+                        </div>
+                    ))}
+                </div>
+
+                <Button label='Reset' onClick={() => reset()} />
+                <Button
+                    label='Complete Modification'
+                    onClick={() => completeMod()}
+                    disabled={selectedCard.values.every(
+                        (value, index) => value === updatedCardValues[index]
+                    )}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default AquaRegia
--- client/src/pages/Forge/components/Modifiers/components/AquaRegiaSuperior/AquaRegiaSuperior.jsx
+++ client/src/pages/Forge/components/Modifiers/components/AquaRegiaSuperior/AquaRegiaSuperior.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { Button, Card } from '@components'
+import { useGlobalContext } from '@context'
+
+import { updateCardValues } from '../../../../api'
 import './AquaRegiaSuperior.scss'
 
-const AquaRegiaSuperior = () => {
-    return <div>AquaRegiaSuperior</div>
+const AquaRegiaSuperior = ({ selectedCard, setModificationComplete }) => {
+    const { getUserCards } = useGlobalContext()
+
+    const [selectedCardValues, setSelectedCardValues] = useState([
+        ...selectedCard.values,
+    ])
+
+    let updatedCardValues = [...selectedCardValues]
+
+    const rotateValues = () => {
+        updatedCardValues.unshift(updatedCardValues.pop())
+        setSelectedCardValues(updatedCardValues)
+    }
+
+    const completeMod = async () => {
+        await updateCardValues(selectedCard, updatedCardValues)
+        await getUserCards()
+        setModificationComplete(true)
+    }
+
+    const reset = () => {
+        setSelectedCardValues(selectedCard.values)
+    }
+
+    return (
+        <div className='Regia center fill'>
+            <div className='start-column'>
+                <Button label='Rotate Values' onClick={() => rotateValues()} />
+                <div className='panel card-select center'>
+                    <div className='selected-card center fill'>
+                        <Card card={selectedCard} isShowing />
+                    </div>
+                    {updatedCardValues?.map((value, i) => (
+                        <div
+                            key={value + i * 10}
+                            className={`value-${i} box center `}
+                        >
+                            {value}
+                        </div>
+                    ))}
+                </div>
+
+                <Button label='Reset' onClick={() => reset()} />
+                <Button
+                    label='Complete Modification'
+                    onClick={() => completeMod()}
+                    disabled={selectedCard.values.every(
+                        (value, index) => value === updatedCardValues[index]
+                    )}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default AquaRegiaSuperior
