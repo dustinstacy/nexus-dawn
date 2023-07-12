37963f6be4e48b04d478d6063e6c55924663316e
Andrew <fleming-andrew@protonmail.com>
2023-07-11 23:16:17 -0400

    fix returns comment

--- client/src/hooks/useToggle.ts
+++ client/src/hooks/useToggle.ts
@@ -4,7 +4,7 @@
  * A reusable hook that manages a boolean toggle state.
  *
  * @param {boolean} initialState - The initial state of the toggle (default: false).
- * @returns {Array} An array containing the toggle state and toggle function.
+ * @returns {Array} An (tuple) array containing the toggle state, toggle function, and a manual toggle state setter.
  *
  * Usage:
  *    const [isOpen, toggleIsOpen, setToggleIsOpen] = useToggle();
