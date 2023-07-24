0a52a0e3ba0d07a3a5c3fce8d3e8970fff62865e
Dustin Stacy <dustin.stacy@outlook.com>
2023-07-24 10:57:47 -0400

    migrate context to ts phase 1

--- client/src/context/GlobalContext.tsx
+++ client/src/context/GlobalContext.tsx
@@ -1,7 +1,34 @@
-import React, { createContext, useContext, useMemo, useReducer } from 'react'
+import React, {
+    ReactNode,
+    createContext,
+    useContext,
+    useMemo,
+    useReducer,
+} from 'react'
 import axios from 'axios'
 
-const initialState = {
+//****** return to declare arrays when global interface is set up *******//
+interface State {
+    user: object | null // User | null
+    fetchingUser: boolean
+    userCards: any[] // Array<Card>
+    userDeck: any[] // Array<Card>
+    allCards: any[] // Array<Card>
+    allItems: any[] // Array<Item>
+    allOpponents: any[] // Array<Opponent>
+}
+
+// Adjust payloads when state types are updated //
+type Action =
+    | { type: 'SET_USER'; payload: object }
+    | { type: 'RESET_USER' }
+    | { type: 'SET_USER_CARDS'; payload: any[] }
+    | { type: 'SET_USER_DECK'; payload: any[] }
+    | { type: 'SET_ALL_CARDS'; payload: any[] }
+    | { type: 'SET_ALL_ITEMS'; payload: any[] }
+    | { type: 'SET_ALL_OPPONENTS'; payload: any[] }
+
+const initialState: State = {
     user: null,
     fetchingUser: true,
     userCards: [],
@@ -11,7 +38,7 @@ const initialState = {
     allOpponents: [],
 }
 
-const globalReducer = (state, action) => {
+const globalReducer = (state: State, action: Action) => {
     switch (action.type) {
         case 'SET_USER':
             return {
@@ -60,7 +87,7 @@ const globalReducer = (state, action) => {
 
 const GlobalContext = createContext(initialState)
 
-export const GlobalProvider = ({ children }) => {
+export const GlobalProvider = ({ children }: { children: ReactNode }) => {
     const [state, dispatch] = useReducer(globalReducer, initialState)
 
     const getGlobalState = async () => {
@@ -175,6 +202,7 @@ export const GlobalProvider = ({ children }) => {
         }
     }
 
+    // @ts-ignore
     const value = useMemo(() => ({
         ...state,
         getGlobalState,
