14317b25beeb59966419b8ddf376ad7273a2e3b9
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-04 20:36:09 -0600

    update directory structure

--- server/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from 'express'
-import { IUser } from '../models/User'
-
-// Type guard function to check if 'req.user' is defined and has IUser type
-export const hasUser = (req: Request): req is Request & { user: IUser } => {
-    return !!req.user
-}
