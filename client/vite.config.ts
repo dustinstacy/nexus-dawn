af20a9bdf79610a31e16d776269e92b813201259
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-06 14:26:16 -0600

    update vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            '@api': path.resolve(__dirname, 'src/api'),
-            '@assets': path.resolve(__dirname, 'src/assets'),
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@constants': path.resolve(__dirname, 'src/constants'),
-            '@context': path.resolve(__dirname, 'src/context/GlobalContext'),
-            '@hooks': path.resolve(__dirname, 'src/hooks/'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@randomizers': path.resolve(__dirname, 'src/utils/randomizers'),
-            '@utils': path.resolve(__dirname, 'src/utils'),
+            "@api": path.resolve(__dirname, "src/api"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@constants": path.resolve(__dirname, "src/constants"),
+            "@context": path.resolve(__dirname, "src/context/GlobalContext"),
+            "@hooks": path.resolve(__dirname, "src/hooks/"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@randomizers": path.resolve(__dirname, "src/utils/randomizers"),
+            "@utils": path.resolve(__dirname, "src/utils"),
         },
     },
     test: {
-        environment: 'jsdom',
+        environment: "jsdom",
     },
     server: {
         proxy: {
-            '/api': {
-                target: 'http://localhost:5000',
+            "/api": {
+                target: "https://nexus-dawn-backend-b310f7b6fbcc.herokuapp.com/",
                 changeOrigin: true,
                 secure: false,
                 ws: true,
