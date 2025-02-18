ff4ecc293f7f93e94ea09d1c63ad9a9d537b621f
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-18 15:57:17 -0600

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ export const metadata: Metadata = {
         icon: [{ url: "/favicon.png" }],
     },
     description: "Nexus Dawn, Triple Triad reborn.",
-    viewport: "width=device-width, initial-scale=1",
 }
 
 export default function RootLayout({
