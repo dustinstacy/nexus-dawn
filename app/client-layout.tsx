1a4b19c9cea8689443d1444218fd66cf716ccf1b
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-18 15:57:54 -0600

    update mounting fetch

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -1,13 +1,35 @@
 "use client"
 
 import { usePathname } from "next/navigation"
+import React, { useEffect } from "react"
+
 import { NavBar } from "@components"
+import { useUserStore } from "@stores"
+import { useItemsStore } from "@stores"
+import { useOpponentsStore } from "@stores"
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname()
+    const checkForUser = useUserStore((state) => state.checkForUser)
+    const allItems = useItemsStore((state) => state.allItems)
+    const allOpponents = useOpponentsStore((state) => state.allOpponents)
+    const fetchItems = useItemsStore((state) => state.fetchItems)
+    const fetchOpponents = useOpponentsStore((state) => state.fetchOpponents)
 
     const isAuthRoute = pathname.startsWith("/auth")
     const isBattleRoute = pathname === "/battle"
+
+    useEffect(() => {
+        checkForUser()
+    }, [])
+
+    useEffect(() => {
+        if (allItems?.length === 0 || allOpponents?.length === 0) {
+            console.log("Fetching items and opponents")
+            fetchItems()
+            fetchOpponents()
+        }
+    }, [])
 
     return (
         <div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import React, { useEffect } from "react"
 
-import { NavBar } from "@components"
 // import { Onboarding } from '@components'
 import { mainPanels, subPanels } from "@constants"
 import { User } from "@interfaces"
-import { useAuthStore, useItemsStore, useOpponentsStore } from "@stores"
+import { useUserStore } from "@stores"
 import { classSet } from "@utils"
 
 import ClientLayout from "./client-layout"
@@ -16,22 +15,7 @@ import ClientLayout from "./client-layout"
 import "./styles/home.scss"
 
 export default function Home() {
-    const checkToken = useAuthStore((state) => state.checkToken)
-    const user = useAuthStore((state) => state.user)
-    const fetchItems = useItemsStore((state) => state.fetchItems)
-    const fetchOpponents = useOpponentsStore((state) => state.fetchOpponents)
-
-    useEffect(() => {
-        checkToken()
-    }, [checkToken])
-
-    useEffect(() => {
-        if (user) {
-            console.log("fetching items")
-            fetchItems()
-            fetchOpponents()
-        }
-    }, [user])
+    const user = useUserStore((state) => state.user)
 
     const stage = user?.onboardingStage ?? {}
 
@@ -39,8 +23,6 @@ export default function Home() {
         classSet(`${className}-${type}`, "panel", "start-column", !user ? "disabled" : "")
 
     const pathname = usePathname()
-
-    console.log("pathname", pathname)
 
     return (
         <ClientLayout>
