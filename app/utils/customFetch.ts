cc65cbe194ccfd02580a528b9b7353f90743228c
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-19 15:02:10 -0600

    update api base url control

--- app/utils/customFetch.ts
+++ app/utils/customFetch.ts
@@ -1,7 +1,7 @@
 // Custom fetch wrapper to handle common fetch logic
 const customFetch = async (url: string, options: RequestInit = {}) => {
     // Set the base URL for the API
-    const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000"
+    const baseUrl = process.env.API_BASE_URL
     const token = sessionStorage.getItem("accessToken")
 
     // Merge the default headers with the options headers
