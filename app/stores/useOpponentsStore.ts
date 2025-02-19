6f84dc9ea8eca055d23bdc747f06c0aa86892ad7
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-18 20:20:36 -0600

    add selectedOpponent and selectedOpponentDeck

--- app/stores/useOpponentsStore.ts
+++ app/stores/useOpponentsStore.ts
@@ -1,15 +1,27 @@
 import { create } from "zustand"
 
-import { IOpponent } from "@interfaces"
+import { ICard, IOpponent } from "@interfaces"
 import { customFetch } from "@utils"
 
 interface OpponentsState {
     allOpponents: IOpponent[]
+    selectedOpponent: IOpponent | null
+    selectedOpponentDeck: Array<ICard> | []
+    setSelectedOpponent: (opponent: IOpponent | null) => void
+    setSelectedOpponentDeck: (deck: Array<ICard>) => void
     fetchOpponents: () => void
 }
 
 const useOpponentsStore = create<OpponentsState>((set) => ({
     allOpponents: [],
+    selectedOpponent: null,
+    selectedOpponentDeck: [],
+    setSelectedOpponent: (opponent) => {
+        set({ selectedOpponent: opponent })
+    },
+    setSelectedOpponentDeck: (deck) => {
+        set({ selectedOpponentDeck: deck })
+    },
     fetchOpponents: async () => {
         try {
             const res = await customFetch("/api/cpuOpponents")
