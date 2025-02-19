4bbfdd6ae796838c7928ceb9a324c97d0fa8bf17
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-18 19:22:36 -0600

    add useCardsStore

--- app/stores/index.ts
+++ app/stores/index.ts
@@ -1,3 +1,4 @@
+export { default as useCardsStore } from "./useCardsStore"
 export { default as useItemsStore } from "./useItemsStore"
 export { default as useOpponentsStore } from "./useOpponentsStore"
 export { default as useUserStore } from "./useUserStore"
---
+++ app/stores/useCardsStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand"
+
+import { ICard } from "@interfaces"
+import { customFetch } from "@utils"
+
+interface CardsState {
+    allCards: ICard[]
+    fetchCards: () => void
+}
+
+const useCardsStore = create<CardsState>((set) => ({
+    allCards: [],
+    fetchCards: async () => {
+        try {
+            const res = await customFetch("/api/cards")
+            console.log(res)
+            set({ allCards: res })
+        } catch (error) {
+            console.error(error)
+        }
+    },
+}))
+
+export default useCardsStore
