3c70af454fec48625e171cecadad285dcdd8fa22
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-24 14:34:26 -0600

    remove unnecessary import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
-import { option } from "framer-motion/client"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
