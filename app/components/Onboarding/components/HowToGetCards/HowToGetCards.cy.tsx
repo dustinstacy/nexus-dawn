e4985c2c6e0db4664a0a5e60eba44f1c1444b6c0
MoarCoding <Stefan.Lisper@forefront.se>
2025-05-19 15:23:16 +0200

    component tests for <HowToGetCards />

---
+++ app/components/Onboarding/components/HowToGetCards/HowToGetCards.cy.tsx
@@ -0,0 +1,65 @@
+import { AppRouterContext } from 'next/dist/shared/lib/app-router-context.shared-runtime'
+import * as Router from 'next/navigation'
+
+import { smlogo } from '@assets'
+import { User } from '@interfaces'
+import stores from '@stores'
+
+import HowToGetCards from './HowToGetCards'
+import { onboardingStages } from '../../constants'
+import { marketMenu, purchaseButton } from '../../images'
+
+const mountComponent = (showStep4 = false) => {
+	const nextStage = cy.stub()
+	const router: any = {
+		push: cy.stub().as('router:push')
+	}
+
+	cy.stub(Router, 'useRouter').returns(router)
+
+	cy.stub(stores, 'useUserStore').returns({
+		onboardingStage: 1,
+		inventory: new Array(showStep4 ? 1 : 0)
+	} as User)
+
+	cy.mount(
+		<AppRouterContext.Provider value={router}>
+			<HowToGetCards nextStage={nextStage} />
+		</AppRouterContext.Provider>
+	)
+}
+
+describe('<HowToGetCards />', () => {
+	it('renders step 1-3', () => {
+		const stageData = onboardingStages[1]
+
+		mountComponent(1)
+
+		cy.getDataCy('header').contains(stageData.header as string)
+
+		// step 1
+		cy.getDataCy('body').contains(stageData.body[0])
+		cy.getDataCy('logo').should('have.attr', 'src', smlogo.src)
+		cy.getDataCy('increment-step-button-1').contains(stageData.label[0]).click()
+
+		// step 2
+		cy.getDataCy('body').contains(stageData.body[1])
+		cy.getDataCy('market-menu-image').should('have.attr', 'src', marketMenu.src)
+		cy.getDataCy('increment-step-button-2').contains(stageData.label[0]).click()
+
+		// step 3
+		cy.getDataCy('body').contains(stageData.body[2])
+		cy.getDataCy('purchase-bar-image').should('have.attr', 'src', purchaseButton.src)
+		cy.getDataCy('close-modal-button').contains(stageData.label[1]).click()
+		cy.getDataCy('modal-overlay').should('not.exist')
+	})
+
+	it('renders step 4', () => {
+		const stageData = onboardingStages[1]
+
+		mountComponent(true)
+
+		cy.getDataCy('body').contains(stageData.body[3])
+		cy.getDataCy('step-4-button').contains(stageData.label[0])
+	})
+})
--- app/components/Onboarding/components/HowToGetCards/HowToGetCards.tsx
+++ app/components/Onboarding/components/HowToGetCards/HowToGetCards.tsx
@@ -12,8 +12,7 @@ import { marketMenu, purchaseButton } from '../../images'
 import './howToGetCards.scss'
 
 const HowToGetCards = ({ nextStage }: NextStage) => {
-	const { useUserStore } = stores
-	const user = useUserStore((state) => state.user)
+	const user = stores.useUserStore((state) => state.user)
 	const { inventory } = (user as User) || {}
 	const stage = user?.onboardingStage
 	const [step, setStep] = useState(1)
@@ -38,58 +37,68 @@ const HowToGetCards = ({ nextStage }: NextStage) => {
 				<ModalOverlay>
 					<div className="get-cards stage around-column">
 						<div className="header-wrapper">
-							<h1 className="header">
+							<h1
+								data-cy="header"
+								className="header"
+							>
 								{onboardingStages[1].header}
 								<img
 									className="logo abs-center"
 									src={smlogo.src}
 									alt="small logo"
+									data-cy="logo"
 								/>
 							</h1>
 						</div>
 						{step === 1 && (
 							<div className="body box start-column">
-								<p>{onboardingStages[1].body[0]}</p>
+								<p data-cy="body">{onboardingStages[1].body[0]}</p>
 								<Button
 									label={onboardingStages[1].label[0]}
 									onClick={incrementStep}
+									dataCy="increment-step-button-1"
 								/>
 							</div>
 						)}
 						{step === 2 && (
 							<div className="body box step-2 center">
-								<p>{onboardingStages[1].body[1]}</p>
+								<p data-cy="body">{onboardingStages[1].body[1]}</p>
 								<img
 									className="market-menu-image"
 									src={marketMenu.src}
 									alt="market menu"
+									data-cy="market-menu-image"
 								/>
 								<Button
 									label={onboardingStages[1].label[0]}
 									onClick={incrementStep}
+									dataCy="increment-step-button-2"
 								/>
 							</div>
 						)}
 						{step === 3 && (
 							<div className="body box center-column">
-								<p>{onboardingStages[1].body[2]}</p>
+								<p data-cy="body">{onboardingStages[1].body[2]}</p>
 								<img
 									className="purchase-bar-image"
 									src={purchaseButton.src}
 									alt="market menu"
+									data-cy="purchase-bar-image"
 								/>
 								<Button
 									label={onboardingStages[1].label[1]}
 									onClick={() => toggleModalOpen()}
+									dataCy="close-modal-button"
 								/>
 							</div>
 						)}
 						{step === 4 && (
 							<div className="body box center-column">
-								<p>{onboardingStages[1].body[3]}</p>
+								<p data-cy="body">{onboardingStages[1].body[3]}</p>
 								<Button
 									label={onboardingStages[1].label[0]}
 									onClick={() => nextStage('/packs')}
+									dataCy="step-4-button"
 								/>
 							</div>
 						)}
