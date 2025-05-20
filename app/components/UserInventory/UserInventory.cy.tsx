81e2ff9febfc1635639f16155f7f8b4d38232122
MoarCoding <Stefan.Lisper@forefront.se>
2025-05-20 16:44:44 +0200

    component tests for <UserInventory />

---
+++ app/components/UserInventory/UserInventory.cy.tsx
@@ -0,0 +1,22 @@
+import { coinImage } from '@assets'
+import { CypressAppRouterContext } from '@cypressUtils'
+import stores from '@stores'
+import UserInventory from './UserInventory'
+
+describe('UserInventory', () => {
+	it('renders correctly', () => {
+		cy.stub(stores, 'useUserStore').returns({
+			coin: 100
+		})
+
+		cy.mount(
+			<CypressAppRouterContext>
+				<UserInventory />
+			</CypressAppRouterContext>
+		)
+
+		cy.getDataCy('user-inventory').should('exist')
+		cy.getDataCy('coin').should('contain', '100')
+		cy.getDataCy('coin-image').should('have.attr', 'src', coinImage.src)
+	})
+})
