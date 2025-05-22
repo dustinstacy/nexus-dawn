d67ddbaec75f420f8ce935d82a0f0d007f0980dc
MoarCoding <Stefan.Lisper@forefront.se>
2025-05-22 12:43:50 +0200

    component tests for <AquaVitae />

--- app/forge/components/CardModification/components/Modifiers/components/AquaVitae/AquaVitae.tsx
+++ app/forge/components/CardModification/components/Modifiers/components/AquaVitae/AquaVitae.tsx
@@ -6,10 +6,10 @@ import { Card } from '@components'
 import { CardValues, ICard } from '@interfaces'
 
 interface AquaVitae {
-	setModificationInProgress: React.Dispatch<React.SetStateAction<boolean>>
 	selectedCard: ICard | null
 	selectedCardValues: Array<number | string>
 	setSelectedCardValues: React.Dispatch<React.SetStateAction<Array<number>>>
+	setModificationInProgress: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const AquaVitae = ({
@@ -54,6 +54,7 @@ const AquaVitae = ({
 
 	const modValueClick = (e: React.MouseEvent<HTMLDivElement>, value: number) => {
 		const target = e.target as Element
+
 		if (chosenValue === null) {
 			target.classList.add('selected')
 			setChosenValue(value as number)
@@ -86,6 +87,7 @@ const AquaVitae = ({
 							key={value + i * 10}
 							className="value box center"
 							onClick={(e) => modValueClick(e, value)}
+							data-cy={`mod-value-${value}`}
 						>
 							{value}
 						</div>
@@ -95,10 +97,12 @@ const AquaVitae = ({
 					<AiOutlineCloseCircle
 						className="cancel"
 						onClick={() => setModificationInProgress(false)}
+						data-cy="cancel-icon"
 					/>
 					<VscDebugRestart
 						className="reset"
 						onClick={() => reset()}
+						data-cy="reset-icon"
 					/>
 					<div className="selected-card center fill">
 						<Card
@@ -111,6 +115,7 @@ const AquaVitae = ({
 							key={(value as number) + i * 10}
 							className={`value-${i} box center ${modValues?.length > 1 && value !== '' && 'disabled'}`}
 							onClick={() => cardValueClick(value, i)}
+							data-cy={`card-value-${value}`}
 						>
 							{value}
 						</div>
