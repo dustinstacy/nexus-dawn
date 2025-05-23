1c38b4d0203068b2c774d1a97364bbabf67fc44f
MoarCoding <Stefan.Lisper@forefront.se>
2025-05-23 16:54:34 +0200

    component tests for <ItemInformation />

--- app/market/components/ItemDisplay/components/ChosenItem/components/ItemInformation/ItemInformation.tsx
+++ app/market/components/ItemDisplay/components/ChosenItem/components/ItemInformation/ItemInformation.tsx
@@ -9,30 +9,49 @@ interface ItemInformation {
 
 // chosenItem: Item currently chosen from market items
 const ItemInformation = ({ chosenItem }: ItemInformation) => {
-	const { useUserStore } = stores
-	const user = useUserStore((state) => state.user)
+	const user = stores.useUserStore((state) => state.user)
 	const { image, info, contents, name } = chosenItem || {}
-
+	console.log(user?.inventory)
 	return (
-		<div className="item-info start">
+		<div
+			className="item-info start"
+			data-cy="item-info"
+		>
 			<div className="item-image">
 				<img
 					src={image}
 					alt={name}
+					data-cy="item-image"
 				/>
-				<div className="owned-inventory">
+				<div
+					className="owned-inventory"
+					data-cy="owned-inventory"
+				>
 					<span>Owned: &nbsp;</span>
 					{user?.inventory.filter((item: IItem) => item?.name === name).length}
 				</div>
 			</div>
 			<div className="item-details between-column">
 				<div className="section">
-					<h2 className="item-name">{name}</h2>
+					<h2
+						className="item-name"
+						data-cy="item-name"
+					>
+						{name}
+					</h2>
 					<hr />
-					<p className="item-desc">{info}</p>
+					<p
+						className="item-desc"
+						data-cy="item-desc"
+					>
+						{info}
+					</p>
 				</div>
 				{contents?.odds && (
-					<div className="item-odds">
+					<div
+						className="item-odds"
+						data-cy="item-odds"
+					>
 						<h4>Odds:</h4>
 						{Object.entries(contents?.odds).map(([key, value]) => (
 							<div key={key}>
